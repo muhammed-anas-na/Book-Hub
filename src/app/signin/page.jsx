40f1d984e675b340cb7c90ca8d1cb4332864e4ad
Muhammed Anas NA <anasna6005@gmail.com>
2024-10-17 18:50:10 +0530

    Chnage signup logo

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -1,5 +1,4 @@
 'use client';
-import Link from "next/link";
 import { auth, provider,signInWithPopup } from "../../../firebase";
 import { useState,useContext } from "react";
 import { UserContext } from "../../../context/userContext";
@@ -46,25 +45,8 @@ export default function Login() {
         <div className="flex h-screen items-center justify-center p-10 overflow-hidden">
             <div className="flex flex-col justify-center items-start md:items-start md:w-1/2 md:ms-32">
                 <h1 className="flex-start font-bold md:text-3xl text-2xl">
-                    Book <span className='bg-orange-400 text-white px-1'>Hub</span></h1>
-                <p className="text-xs text-gray-500">Increase your collabration and engagment</p>
-                {/* <label htmlFor="email" className="mt-8 text-sm font-semibold">Email</label>
-                <input 
-                type="text" 
-                name="email"
-                className="border rounded-lg focus:outline-0 focus:border-green-800 w-72 p-1" />
-
-                <label htmlFor="Description" className="mt-4 text-sm font-semibold">Password</label>
-                <input 
-                type="password" 
-                name="password"
-                className="border rounded-lg focus:outline-0 focus:border-green-800 w-72 p-1" />
-
-                <button className="border mt-4 w-72 py-1 bg-green-800 text-white rounded-lg">Login</button>
-                <Link 
-                href={'/signup'}
-                className="text-xs text-gray-500 mt-2 md:ms-20 ms-14 w-full"
-                >New to our platform?</Link> */}
+                    Read <span className='bg-[#F76B19] text-white px-1'>In</span></h1>
+                <p className="text-xs text-gray-500">Connect with the community of Book lovers</p>
                 <button 
                 onClick={handleSignIn}
                 className="md:w-72 my-5 bg-black text-white px-6 py-3 rounded-lg shadow-lg hover:bg-gray-800 flex gap-3">
